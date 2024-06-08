5f5f198e47e25da280d8dda0d142c5aeb9a9aba6
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-06-08 12:57:42 +0200

    styled sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,19 +1,40 @@
-<nav id="sidebar" class="navbar-dark">
-  <a href="/" class="nav-link text-white">
-    <h2 class="p-2 ldg-text-black"><i class="fa-solid fa-square-rss ldg-text-black"></i> Administration</h2>
-  </a>
-  <ul class="nav flex-column">
-    <li class="nav-item">
-      <a class="nav-link ldg-text-black {{Route::currentRouteName() == 'admin.dashboard' ? 'active' : ''}}" href="{{route('admin.dashboard')}}"><i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i>Dasboard</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link ldg-text-black  {{Route::currentRouteName() == 'admin.projects.index' ? 'active' : ''}}" href="{{route('admin.projects.index')}}"> <i class="fa-solid fa-newspaper fa-lg fa-fw"></i>Projects</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link ldg-text-black {{Route::currentRouteName() === 'admin.categories.index' ? 'active' : ''}}" href="{{ route('admin.categories.index') }}"><i class="fa-solid fa-book-open fs-4 pe-3"></i><span class="hype-text-collapse">Categories</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link ldg-text-black {{Route::currentRouteName() === 'admin.technologies.index' ? 'active' : ''}}" href="{{ route('admin.technologies.index') }}"><i class="fa-solid fa-microchip"></i><span class="hype-text-collapse">Technologies</span></a>
-    </li>
-  </ul>
+<nav class="navbar-dark">
+  <div id="sidebar" class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+    <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <span class="fs-4"><i class="fa-solid fa-square-rss"></i> Administration</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link {{Route::currentRouteName() == 'admin.index' ? 'active' : ''}}" aria-current="page"><i class="fa-solid fa-house-user"></i> Home</a>
+      </li>
+      <li>
+        <a href="{{route('admin.dashboard')}}" class="nav-link text-white {{Route::currentRouteName() == 'admin.dashboard' ? 'active' : ''}}"><i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard</a>
+      </li>
+      <li>
+        <a href="{{route('admin.projects.index')}}" class="ldg-hover nav-link text-white {{Route::currentRouteName() == 'admin.projects.index' ? 'active' : ''}}"><i class="fa-solid fa-newspaper fa-lg fa-fw"></i> Projects</a>
+      </li>
+      <li>
+        <a href="{{ route('admin.categories.index') }}" class="nav-link text-white {{Route::currentRouteName() === 'admin.categories.index' ? 'active' : ''}}"><i class="fa-solid fa-book-open fs-5"></i> Categories</a>
+      </li>
+      <li>
+        <a href="{{ route('admin.technologies.index') }}" class="nav-link text-white {{Route::currentRouteName() === 'admin.technologies.index' ? 'active' : ''}}"><i class="fa-solid fa-microchip fs-5"></i> Technologies</a>
+      </li>
+    </ul>
+    <hr>
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="" width="32" height="32" class="rounded-circle me-2">
+        <strong>{{Auth::user()->name}}</strong>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+        <li><a class="dropdown-item" href="{{route('admin.projects.create')}}">New project</a></li>
+        <li><a class="dropdown-item" href="{{route('profile.edit')}}">Profile</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" title="Logout">Sign out</a></li>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
+      </ul>
+  </div>
 </nav>
