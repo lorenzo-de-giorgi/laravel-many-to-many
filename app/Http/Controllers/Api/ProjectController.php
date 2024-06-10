02f7d0d74bf3e26aa669e7e88536700a6f14bc1b
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-06-10 15:21:19 +0200

    added api

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        // dd($projects);
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::where('slug', $slug)->with('category', 'technologies')->first();
+        if($project){
+            return response()->json([
+                'success'=> true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Post not found'
+            ]);
+        }
+    }
+}
